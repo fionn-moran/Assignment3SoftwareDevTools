33847eb99298d31c5452f754fe7c1de9bb87f800
fionnmoran <fionnmoran13@gmail.com>
2023-04-27 14:05:54 +0100

    Add updateCoffeeShop function to CoffeeShopAPI - to update a previously added shop

--- src/main/kotlin/controllers/CoffeeShopAPI.kt
+++ src/main/kotlin/controllers/CoffeeShopAPI.kt
@@ -19,7 +19,22 @@ class CoffeeShopAPI {
         } else null
     }
 
+    // updates a previously added note with additional/changed details
+    fun updateCoffeeShop(indexToUpdate: Int, coffeeShop: CoffeeShop?): Boolean {
+        //find the coffee shop by the index number
+        val foundCoffeeShop = findCoffeeShop(indexToUpdate)
 
+        //if the coffee shop exists, use the coffee shop details passed as parameters to update the found coffee shop in the ArrayList.
+        if ((foundCoffeeShop != null) && (coffeeShop != null)) {
+            foundCoffeeShop.shopName = coffeeShop.shopName
+            foundCoffeeShop.shopLocation = coffeeShop.shopLocation
+            foundCoffeeShop.shopDetails = coffeeShop.shopDetails
+            foundCoffeeShop.dateAdded = coffeeShop.dateAdded
+            return true
+        }
 
+        //if the coffee shop was not found, return false, indicating that the update was not successful
+        return false
+    }
 
 }
