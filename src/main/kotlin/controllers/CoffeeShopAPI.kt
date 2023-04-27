cc7fa0e693bfa6374f5e76fa2bb81544c252902f
fionnmoran <fionnmoran13@gmail.com>
2023-04-27 13:41:02 +0100

    Add function to add new coffee shop to coffee shop array list

--- src/main/kotlin/controllers/CoffeeShopAPI.kt
+++ src/main/kotlin/controllers/CoffeeShopAPI.kt
@@ -1,4 +1,13 @@
 package controllers
 
+import models.CoffeeShop
+
 class CoffeeShopAPI {
+
+    private var coffeeShops = ArrayList<CoffeeShop>()
+
+    // adds a new coffee shop to coffee shop array list
+    fun add(coffeeShop: CoffeeShop): Boolean {
+        return coffeeShops.add(coffeeShop)
+    }
 }
